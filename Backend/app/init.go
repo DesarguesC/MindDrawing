d765d4c2fc24ad7d9aa947f5014b66d2b5ded864
Chandy0527 <425593160@qq.com>
2023-04-22 18:00:35 +0800

    web init

--- Backend/app/init.go
+++ Backend/app/init.go
@@ -1,3 +1,20 @@
 package app
 
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
 // init web service
+
+var e *echo.Echo
+
+func InitWebFramework() {
+	e = echo.New()
+	e.HideBanner = true
+	//addRoutes()
+	logrus.Info("echo framework initialized")
+}
+func StartServer() {
+	e.Logger.Fatal(e.Start(":3000"))
+}
--- Backend/app/route.go
+++ Backend/app/route.go
@@ -2,3 +2,16 @@ package app
 
 // add web routes
 
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func ping(c echo.Context) error {
+	return c.String(200, "pong!")
+}
+
+func AddRoutes() {
+	//visit := e.Group("visit", middleware)
+	api := e.Group("api")
+	api.GET("/ping", ping)
+}
