4d0305a43505e01a090ba018bc188624428926f9
Chandy0527 <425593160@qq.com>
2023-04-22 19:19:55 +0800

    move

--- Backend/app/controller/UserOperation.go
+++ Backend/app/controller/UserOperation.go
@@ -1,3 +1,24 @@
 package controller
 
+import (
+	"Backend/app/response"
+	"github.com/labstack/echo/v4"
+)
+
+var status string
+
 // 从前端bind数据到user.dto结构，调用该部分的crud
+func Users_Judge(c echo.Context) error {
+	if status == "nil" {
+		return response.SendResponse(c, -999, "请先登录")
+	}
+	return response.SendResponse(c, -900, "登录状态正常")
+}
+
+func Users_Register(c echo.Context) error {
+	if status != "nil" {
+		return Users_Judge(c)
+	}
+	data := new(model.)
+
+}
