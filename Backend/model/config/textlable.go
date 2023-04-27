19667ffbc8945a16dba17bf4578613be32e7bd7c
Chandy0527 <425593160@qq.com>
2023-04-27 19:46:44 +0800

    model

--- Backend/model/config/textlable.go
+++ Backend/model/config/textlable.go
@@ -9,19 +9,19 @@ import (
 
 const LENGTH = 10
 
-func Get_labels() (model.Strs, error) {
+func Get_labels() (model.Strs, int, error) {
 	viper.SetConfigName("labels")
 	viper.SetConfigType("yaml") // yaml also be fine
 	viper.AddConfigPath("./model/config/")
 	// 数据库用户登录的配置文件直接放在short-link-backend文件夹内
 	if err := viper.ReadInConfig(); err != nil {
 		logrus.Panic("Error occurred when fetching text labels")
-		return model.Strs{}, err
+		return model.Strs{}, LENGTH, err
 	}
 	Label := viper.GetStringMapString("labels")
 	labels := new(model.Strs)
 	for i := 1; i <= LENGTH; i++ {
 		(*labels) = append((*labels), Label["choice_"+strconv.Itoa(i)])
 	}
-	return *labels, nil
+	return *labels, LENGTH, nil
 }
--- Backend/model/dto/text.dto.go
+++ Backend/model/dto/text.dto.go
@@ -1,11 +1,18 @@
 package dto
 
-import "mime/multipart"
+import (
+	"Backend/model"
+	"mime/multipart"
+)
 
 type bools []bool
 
 type TextInput struct {
 	Text string `gorm:"varchar(600)" form:"text" json:"text"`
+}
+
+type TextLabelInput struct {
+	LabelList model.Strs `gorm:"longtext" form:"label_list" json:"label_list"`
 }
 
 type TextFileUpload struct {
