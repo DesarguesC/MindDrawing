3f8d91ef9e7fe70b705b36f871fc9821367912a4
Chandy0527 <425593160@qq.com>
2023-04-27 19:15:33 +0800

    add story names

--- Backend/model/HisDefine.go
+++ Backend/model/HisDefine.go
@@ -10,6 +10,7 @@ type History_Create struct {
 	User_Id   int  `gorm:"type:uint" form:"user_id" json:"user_id"`
 	Text_path Strs `gorm:"type:longtext" form:"user_text_path" json:"user_text_path,omitempty"`
 	Img_path  Strs `gorm:"type:longtext" form:"user_img_path" json:"user_img_path,omitempty"`
+	Names     Strs `gorm:"type:longtext" form:"user_story_names" json:"user_story_names,omitempty"`
 }
 
 /*
@@ -31,6 +32,7 @@ func (m *History_Create) Scan(val interface{}) error {
 	(*m).User_Id, err = strconv.Atoi(ss[0])
 	(*m).Text_path = strings.Split(ss[1], "|")
 	(*m).Img_path = strings.Split(ss[2], "|")
+	(*m).Names = strings.Split(ss[3], "|")
 	return err
 }
 
@@ -38,6 +40,7 @@ func (m History_Create) Value() (driver.Value, error) {
 	str_1 := strconv.Itoa(m.User_Id)
 	str_2 := strings.Join(m.Text_path, "|")
 	str_3 := strings.Join(m.Img_path, "|")
-	str := str_1 + ";" + str_2 + ";" + str_3
+	str_4 := strings.Join(m.Names, "|")
+	str := str_1 + ";" + str_2 + ";" + str_3 + ";" + str_4
 	return str, nil
 }
--- Backend/model/model.go
+++ Backend/model/model.go
@@ -24,7 +24,7 @@ type MD_User struct {
 /*
 read example:
 
-10 & Name & Email & Pwd & SecQ & SecA & 10;./TEXT/1/|./TEXT/2/;./IMAGE/1/|./IMAGE/2/ & Lastlogin
+10 & Name & Email & Pwd & SecQ & SecA & 10; ./TEXT/1/|./TEXT/2/; ./IMAGE/1/|./IMAGE/2/; name1|name2 & Lastlogin
 
 */
 
@@ -49,6 +49,8 @@ func (m *MD_User) Scan(val interface{}) error {
 	}
 	(*m).His_Cre.Text_path = strings.Split(sss[1], "|")
 	(*m).His_Cre.Img_path = strings.Split(sss[2], "|")
+	(*m).His_Cre.Names = strings.Split(sss[3], "|")
+
 	(*m).LastLogin, _ = time.Parse(ss[7], "2006-01-02 15:04:05")
 	// string -> time
 
@@ -65,7 +67,7 @@ store example:
 func (m MD_User) Value() (driver.Value, error) {
 	str_1 := strconv.Itoa(m.Id)
 	str_2 := m.Name + "&" + m.Email + "&" + m.Pwd + "&" + m.SecQ + "&" + m.SecA
-	str_3 := strconv.Itoa(m.His_Cre.User_Id) + ";" + strings.Join(m.His_Cre.Text_path, "|") + ";" + strings.Join(m.His_Cre.Img_path, "|")
+	str_3 := strconv.Itoa(m.His_Cre.User_Id) + ";" + strings.Join(m.His_Cre.Text_path, "|") + ";" + strings.Join(m.His_Cre.Img_path, "|") + ";" + strings.Join(m.His_Cre.Names, "|")
 	str_4 := m.LastLogin.Format("2006-01-02 15:04:05")
 	// time -> string
 
