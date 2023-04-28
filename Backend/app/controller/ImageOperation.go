dbdbc38a6583c2e20983ae0e0e7077f5b7882f62
Chandy0527 <425593160@qq.com>
2023-04-28 13:54:17 +0800

    status

--- Backend/app/controller/ImageOperation.go
+++ Backend/app/controller/ImageOperation.go
@@ -1,19 +1,30 @@
 package controller
 
-import "github.com/labstack/echo/v4"
+import (
+	"Backend/app/response"
+	"github.com/labstack/echo/v4"
+)
 
 func StyleImage_Upload(c echo.Context) error {
-
+	if Status == "nil" {
+		return response.SendResponse(c, -999, "请先登录")
+	}
 }
 
 func Get_GenImages(c echo.Context) error {
-
+	if Status == "nil" {
+		return response.SendResponse(c, -999, "请先登录")
+	}
 }
 
 func SketchImage_Upload(c echo.Context) error {
-
+	if Status == "nil" {
+		return response.SendResponse(c, -999, "请先登录")
+	}
 }
 
 func EditImage(c echo.Context) error {
-
+	if Status == "nil" {
+		return response.SendResponse(c, -999, "请先登录")
+	}
 }
--- Backend/app/controller/TextOperation.go
+++ Backend/app/controller/TextOperation.go
@@ -1,23 +1,36 @@
 package controller
 
-import "github.com/labstack/echo/v4"
+import (
+	"Backend/app/response"
+	"github.com/labstack/echo/v4"
+)
 
 func Text_Input_Upload(c echo.Context) error {
-	
+	if Status == "nil" {
+		return response.SendResponse(c, -999, "请先登录")
+	}
 }
 
 func TextFile_Upload(c echo.Context) error {
-
+	if Status == "nil" {
+		return response.SendResponse(c, -999, "请先登录")
+	}
 }
 
 func TextLabel_Upload(c echo.Context) error {
-
+	if Status == "nil" {
+		return response.SendResponse(c, -999, "请先登录")
+	}
 }
 
 func Get_GenTexts(c echo.Context) error {
-
+	if Status == "nil" {
+		return response.SendResponse(c, -999, "请先登录")
+	}
 }
 
 func Editor_TextGuidance(c echo.Context) error {
-
+	if Status == "nil" {
+		return response.SendResponse(c, -999, "请先登录")
+	}
 }
